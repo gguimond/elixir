2a2194342a54ff2878c00c54bc44df2782dd67c7
Guillaume Guimond <guimog@gmail.com>
2018-05-25 15:31:07 +0200

    test

--- example/test/example_test.exs
+++ example/test/example_test.exs
@@ -2,7 +2,44 @@ defmodule ExampleTest do
   use ExUnit.Case
   doctest Example
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "greets the world" do
+    assert Example.hello() == :world
+  end
+end
+
+defmodule SendingProcess do
+  def run(pid) do
+    send(pid, :ping)
+  end
+end
+
+defmodule TestReceive do
+  use ExUnit.Case
+
+  test "receives ping" do
+    SendingProcess.run(self())
+    assert_received :ping
+  end
+end
+
+defmodule OutputTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "outputs Hello World" do
+    assert capture_io(fn -> IO.puts("Hello World") end) == "Hello World\n"
+  end
+end
+
+defmodule ExampleTest2 do
+  use ExUnit.Case
+  doctest Example
+
+  setup_all do
+    {:ok, recipient: :world}
+  end
+
+  test "greets", state do
+    assert Example.hello() == state[:recipient]
   end
 end
